98cf2b921b67acd3399a2a505c535bdd83922ce2
Derick Olson <olsonderick@gmail.com>
2023-06-15 16:24:49 -0400

    get /ask working for test data

--- app/controllers/api/v1/ask_controller.rb
+++ app/controllers/api/v1/ask_controller.rb
@@ -1,6 +1,7 @@
 require 'json'
 require 'openai'
 require 'polars'
+require 'matrix'
 
 OPENAI_API_KEY = ENV.fetch('OPENAI_API_KEY')
 EMBEDDINGS_MODEL = 'text-embedding-ada-002'.freeze
@@ -20,9 +21,8 @@ class Api::V1::AskController < ApplicationController
 
     # TODO: lookup previous questions
     prompt = generate_prompt(question)
-    puts prompt
+    puts "Prompt:\n\n #{prompt}\n\n"
     answer = get_completion(prompt)
-
     # TODO: cache answer
 
     render json: { question: question, answer: answer }
@@ -32,26 +32,30 @@ class Api::V1::AskController < ApplicationController
 
   # TODO: move this to a helper
   def generate_prompt(question)
-    # TODO: get get embeddings for question
-    question_vector = get_embedding(question)
-    pages_df = Polars.read_csv('book.pdf.pages.csv')
+    # Get embeddings
+    question_embeddings = Vector.elements(get_embedding(question))
     embeddings_df = Polars.read_csv('book.pdf.embeddings.csv')
 
-    puts pages_df.head(5)
-    puts embeddings_df.head(5)
+    # Find the most similar page title to the question
+    similarity_scores = []
+    embeddings_df.iter_rows do |row|
+      page_embeddings = Vector.elements(row[1..row.length])
+      score = question_embeddings.dot(page_embeddings) # both are vectors of embeddings
+      similarity_scores << score
+    end
+    embeddings_df.hstack([Polars::Series.new('scores', similarity_scores)], in_place: true)
+    embeddings_df.sort!('scores', reverse: true)
 
-    # get the answer:
+    best_matching_title = embeddings_df.head(1)['title'][0]
 
-    # put the following in a block comment
-    ###
-    # 1. turn question into embeddings [1,2,3,...n]
-    # 2. get similarity between question_embedding and list from doc_embeddings_df
-    # 3. look up the page content from pages_df (this does not need to be a dataframe...) using top similar embeddings's page number
-    # construct prompt using template, page content, and question
+    puts "adding context: #{best_matching_title}"
+
+    # Look up the content for that page
+    pages_df = Polars.read_csv('book.pdf.pages.csv')
+    content = pages_df.filter(Polars.col('title') == best_matching_title).head['content'][0]
 
     preface = 'Flights is a 2007 fragmentary novel by the Polish author Olga Tokarczuk, who won the 2018 Nobel Prize in Literature. These questions and answers are based on someone who has read the book.\n\n'
     directions = 'Please keep your answers to three sentences maximum, and speak in complete sentences. Stop speaking once your point is made.\n\nContext that may be useful, pulled from Flights:\n'
-    content = 'TODO'
 
     preface + directions + "Here is some context: #{content}\n" + question
   end
