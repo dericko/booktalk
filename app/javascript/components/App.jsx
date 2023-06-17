7746bb6c390b23e37ad7b443ea9d3b2f123ffa0c
Derick Olson <olsonderick@gmail.com>
2023-06-17 18:06:36 -0400

    feat: add some styles to look like askmybook.com

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -32,17 +32,29 @@ const App = () => {
 
   return (
     <div className="AppContainer">
-      <h1>Ask a book</h1>
-      <label>Question</label>
-      <textarea
-        value={inputValue}
-        onChange={handleOnChange}
-        rows={6}
-        cols={36}
-        pattern="\w+"
-      />
-      <button onClick={handleAskQuestion}>Ask</button>
-      <div>{answer}</div>
+      <div className="HeadingContainer">
+        <a href="https://www.amazon.com/Minimalist-Entrepreneur-Great-Founders-More/dp/0593192397">
+          <img src="./assets/tme_cover.png" alt="book cover" />
+        </a>
+        <h1>Ask My Book</h1>
+      </div>
+      <div className="AskContainer">
+        <label className="Description">
+          This is an experiment in using AI to make my book's content more
+          accessible. Ask a question and AI'll answer it in real-time:
+        </label>
+
+        <textarea value={inputValue} onChange={handleOnChange} pattern="\w+" />
+        <div className="Buttons">
+          <button onClick={handleAskQuestion}>Ask Question</button>
+          <button disabled onClick={() => console.log("todo")}>I'm Feeling Lucky</button>
+        </div>
+        <div className="AnswerContainer">{answer}</div>
+      </div>
+      <label className="Credits">
+        Based on <a href="https://askmybook.com">askmybook.com</a> |
+        View source: <a href="https://github.com/dericko/rera-askmybook">github.com/dericko</a>
+      </label>
     </div>
   );
 };
