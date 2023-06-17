53bb858513373ee768dfbdca6b73b686e19cef6b
Derick Olson <olsonderick@gmail.com>
2023-06-17 19:15:15 -0400

    fix lucky button

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -2,12 +2,17 @@ import React, { useState } from "react";
 import "./App.css";
 
 const API = "/api/v1";
-const defaultQuestions = ["What is a minimalist entrepreneur?", "What is your definition of community?", "How do I decide what kind of business I should start?"];
-const getDefaultQuestion = () => defaultQuestions[Math.floor(Math.random() * defaultQuestions.length)];
+const defaultQuestions = [
+  "What is a minimalist entrepreneur?",
+  "What is your definition of community?",
+  "How do I decide what kind of business I should start?",
+];
+const getRandomQuestion = () =>
+  defaultQuestions[Math.floor(Math.random() * defaultQuestions.length)];
 
 const App = () => {
   const [answer, setAnswer] = useState("");
-  const [inputValue, setInputValue] = useState(getDefaultQuestion());
+  const [inputValue, setInputValue] = useState(getRandomQuestion());
 
   const isLoading = answer === "Loading...";
 
@@ -15,7 +20,8 @@ const App = () => {
     setInputValue(event.target.value);
   };
 
-  const handleAskQuestion = async () => {
+  const handleAskQuestion = async (question) => {
+    setInputValue(question);
     setAnswer("Loading...");
     const token = document.querySelector('meta[name="csrf-token"]').content;
     try {
@@ -25,7 +31,7 @@ const App = () => {
           "Content-Type": "application/json",
           "X-CSRF-Token": token,
         },
-        body: JSON.stringify({ question: inputValue }),
+        body: JSON.stringify({ question }),
       });
       const data = await response.json();
       setAnswer(data.answer);
@@ -38,7 +44,10 @@ const App = () => {
     <div className="AppContainer">
       <div className="HeadingContainer">
         <a href="https://www.amazon.com/Minimalist-Entrepreneur-Great-Founders-More/dp/0593192397">
-          <img src="./assets/tme_cover.png" alt="Cover image for the book The Minimalist Entrepreneur" />
+          <img
+            src="./assets/tme_cover.png"
+            alt="Cover image for the book The Minimalist Entrepreneur"
+          />
         </a>
         <h1>Ask My Book</h1>
       </div>
@@ -48,16 +57,38 @@ const App = () => {
           accessible. Ask a question and AI'll answer it in real-time:
         </label>
 
-        <textarea className="QuestionInput" value={inputValue} onChange={handleOnChange} pattern="\w+" />
+        <textarea
+          className="QuestionInput"
+          value={inputValue}
+          onChange={handleOnChange}
+          pattern="\w+"
+        />
         <div className="Buttons">
-          <button className="AskButton" isabled={isLoading} onClick={handleAskQuestion}>Ask Question</button>
-          <button className="LuckyButton" disabled={isLoading} onClick={() => console.log("todo")}>I'm Feeling Lucky</button>
+          <button
+            className="AskButton"
+            disabled={isLoading}
+            onClick={() => handleAskQuestion(inputValue)}
+          >
+            Ask Question
+          </button>
+          <button
+            className="LuckyButton"
+            disabled={isLoading}
+            onClick={() => {
+              handleAskQuestion(getRandomQuestion());
+            }}
+          >
+            I'm Feeling Lucky
+          </button>
         </div>
         <div className="AnswerContainer">{answer}</div>
       </div>
       <label className="Credits">
-        Based on <a href="https://askmybook.com">askmybook.com</a> |
-        View source: <a href="https://github.com/dericko/rera-askmybook">github.com/dericko</a>
+        Based on <a href="https://askmybook.com">askmybook.com</a> | View
+        source:{" "}
+        <a href="https://github.com/dericko/rera-askmybook">
+          github.com/dericko
+        </a>
       </label>
     </div>
   );
